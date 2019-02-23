4296bb09509e4b2921fe044ea1a1c2b7b2fa0227
xuanfa <zhang_xianfa@tom.com>
2019-02-23 03:13:33 +0000

    add saveloadmodel

---
+++ study/saveloadmodel.py
@@ -0,0 +1,123 @@
+import tensorflow as tf
+from tensorflow import keras
+import os
+
+#print cmd execute info to console
+def execcmd(cmdstr):
+    strlines = os.popen(cmdstr).readlines()
+    print('\ncmdstr:' + cmdstr + '\n')
+    for line in strlines:
+        print(line, end='')
+    print('\n')
+
+
+# Returns a short sequential model
+def create_model():
+    model = tf.keras.models.Sequential([
+        keras.layers.Dense(512, activation=tf.nn.relu, input_shape=(784,)),
+        keras.layers.Dropout(0.2),
+        keras.layers.Dense(10, activation=tf.nn.softmax)
+        ])
+
+    model.compile(optimizer=tf.keras.optimizers.Adam(),
+    loss=tf.keras.losses.sparse_categorical_crossentropy,
+    metrics=['accuracy'])
+    return model
+
+if __name__ == '__main__':
+    #prehandle data
+    (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+    
+    train_labels = train_labels[:1000]
+    test_labels = test_labels[:1000]
+
+    train_images = train_images[:1000].reshape(-1, 28 * 28) / 255.0
+    test_images = test_images[:1000].reshape(-1, 28 * 28) / 255.0
+
+    # Create a basic model instance show summary info
+    model = create_model()
+    model.summary()
+
+    #save training process use ModelCheckpoint
+    print('\n**********Test save training process use ModelCheckpoint')
+    checkpoint_path = "training_1/cp.ckpt"
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+
+    # Create checkpoint callback
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(
+        checkpoint_path,
+        save_weights_only=True,
+        verbose=1)
+
+    model = create_model()
+    model.fit(
+        train_images,
+        train_labels,
+        epochs = 10,
+        validation_data = (test_images,test_labels),
+        callbacks = [cp_callback])  # pass callback to training
+
+    execcmd('ls -lh ' + checkpoint_dir)
+
+    model = create_model()
+    loss, acc = model.evaluate(test_images, test_labels)
+    print("Untrained model, accuracy: {:5.2f}%".format(100*acc))
+
+    model.load_weights(checkpoint_path)
+    loss,acc = model.evaluate(test_images, test_labels)
+    print("Restored model, accuracy: {:5.2f}%".format(100*acc))
+
+    #save training process use ModelCheckpoint and set the period option
+    print('\n**********Test save training process use ModelCheckpoint and set the period option')
+    # include the epoch in the file name. (uses `str.format`)
+    checkpoint_path = "training_2/cp-{epoch:04d}.ckpt"
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(
+        checkpoint_path, verbose=1, save_weights_only=True,
+        # Save weights, every 5-epochs.
+        period=5)
+
+    model = create_model()
+    model.fit(
+        train_images,
+        train_labels,
+        epochs = 50,
+        callbacks = [cp_callback],
+        validation_data = (test_images,test_labels),
+        verbose=0)
+    execcmd('ls -lh ' + checkpoint_dir)
+
+    #test load latest checkpoint
+    print('\n**********Test load latest checkpoint')
+    model = create_model()
+    latest = tf.train.latest_checkpoint(checkpoint_dir)
+    model.load_weights(latest)
+    loss, acc = model.evaluate(test_images, test_labels)
+    print("Restored model, accuracy: {:5.2f}%".format(100*acc))
+
+    # Save the weights
+    model.save_weights('./checkpoints/my_checkpoint')
+
+    # Restore the weights
+    model = create_model()
+    model.load_weights('./checkpoints/my_checkpoint')
+
+    loss,acc = model.evaluate(test_images, test_labels)
+    print("Restored model, accuracy: {:5.2f}%".format(100*acc))
+
+    #save entire model test
+    print('\n**********Test save entire model')
+    model = create_model()
+    model.fit(train_images, train_labels, epochs=5)
+
+    # Save entire model to a HDF5 file
+    model.save('my_model.h5')
+
+    # Recreate the exact same model, including weights and optimizer.
+    new_model = keras.models.load_model('my_model.h5')
+    new_model.summary()
+
+    loss, acc = new_model.evaluate(test_images, test_labels)
+    print("Restored model, accuracy: {:5.2f}%".format(100*acc))
+
